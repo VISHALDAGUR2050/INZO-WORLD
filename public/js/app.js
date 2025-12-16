95c3f1b0022402de61245e605c1ba284c4fb5265
VISHALDAGUR2050 <dagurrahul775@gmail.com>
2025-12-16 17:38:31 +0000

    Add frontend pages

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+function goLogin() {
+  location.href = "/login.html";
+}
+
+function login() {
+  alert("Login flow will connect with Supabase next");
+  location.href = "/profile.html";
+}
+
+function register() {
+  alert("Register flow will connect with Supabase next");
+  location.href = "/login.html";
+}
+
+function logout() {
+  location.href = "/login.html";
+}
